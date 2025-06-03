99f6543c4a27bfff3caee657c3d3a56f68db86c1
Joao Jorge <joaojorgedosanjos@gmail.com>
2025-06-03 01:17:39 -0300

    first commit

---
+++ 02-declaracao-atribuicao/main.go
@@ -0,0 +1,16 @@
+package main
+
+const a = "Hello, world"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "nome"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X" //var a string
+	a = "Z"  //sem : quando for reatribuir, := só na primeiravez
+	println(a)
+}
